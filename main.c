146a68bb20a241ba8ca19b60cb75f3a003747caa
Danny <33044223+danielenricocahall@users.noreply.github.com>
2018-03-10 14:53:51 -0500

    Update main.c
    Update!

--- main.c
+++ main.c
@@ -3,30 +3,28 @@
 #include "tempSensor.h"
 #include "FlashMemory.h"
 #include "I2C.h"
-
+#include <math.h>
 //***//***************************************************************************************
 //  FireFly
 //
 //  Description; Using internal temperature sensor, and MPU-6050 Accelerometer,
 //  record real time data to flash memory
 //  Sampling Rate: 100 Hz
-//  Memory allocated: 12 KB
+//  Memory allocated: ~12 KB
 //
 //  Daniel Cahall, Christopher Frederickson, Craig Szot
 //  Rowan University
 //  December 2015
 //  Built with Code Composer Studio v5
 //***************************************************************************************
-int xAccel;
-int yAccel;
 int zAccel;
-int xAngle;
+int yAccel;
+int xAccel;
+int zAngle;
 int yAngle;
 int zAngle;
 int temp;
-int tempX;
-int hum;
-int pressure;
+
  int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
@@ -34,22 +32,35 @@ int pressure;
     DCOCTL  = CALDCO_1MHZ;
     initMPU6050();
     tempInit();
+    int temp_predicted = tempOut();
     //infinite loop
     while(1)
     {
-    	//__delay_cycles(10000);
     	getMPU6050();//Updates acceleration values
-    	hum = getHumidity();
-    	tempX = getTemp();
-    	pressure = getPressure();
-    	__no_operation();
+    	zAccel = getZAccel();
+    	yAccel = getYAccel();
+    	xAccel = getXAccel();
+    	zAngle = getZAngle();
+    	yAngle = getYAngle();
+    	xAngle = getXAngle();
+	temp = getTemprature();
+
+    	//__no_operation();
+
     	//maximum threshold acceleration value, assuming sensitivity is +/- 16gs
-    	if(getZAccel() > 2048)
+    	if(zAccel > 1*1024)
     	{
     		//this is for writing temperature and acceleration to flash
-    		//Although this can be modified to write pressure, etc.
+    		//Although this can be modified to write other quantites
     		char *Flash_ptr = (char *)0xC714; 		//Flash pointer starting position - this is determined based on where your code ends in memory
     		write_flash(Flash_ptr);					//Call function to clear/write temp to flash
     	}
     }
+
 }
+
+
+
+
+
+
